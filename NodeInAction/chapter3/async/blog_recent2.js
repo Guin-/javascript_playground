45d15d8894b885b4530f004841416d714f3182ff
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-18 16:54:35 -0400

    NIA ch3: refactor callback example

---
+++ NodeInAction/chapter3/async/blog_recent2.js
@@ -0,0 +1,34 @@
+// Refactor blog_recent1.js to reduce nesting caused by if/else blocks
+// return early from functions
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    getTitles(res);
+}).listen(8000, '127.0.0.1');
+
+function getTitles(res){
+    fs.readFile('./titles.json', function(err, data){
+        if (err) { return hadError(res)}
+        getTemplate(JSON.parse(data.toString(), res))
+    })
+}
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', function(err, data) {
+        if (err) { return hadError(err, res)}
+        formatHtml(titles, data.toString(), res)
+    })
+}
+
+function formatHtml(titles, tmpl, res) {
+    var html = tmpl.replace('%', titles.join('</li></li>'));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
+
+function hadError(err, res){
+    console.log(err);
+    res.end('Server Error')
+}
