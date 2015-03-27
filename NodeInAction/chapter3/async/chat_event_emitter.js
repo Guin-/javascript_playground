9a6e227f534fcbb3a5a17e724d96897f748b8741
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-27 14:01:22 -0400

    add shutdown and listener count functionality

--- NodeInAction/chapter3/async/chat_event_emitter.js
+++ NodeInAction/chapter3/async/chat_event_emitter.js
@@ -15,19 +15,60 @@ channel.on('join', function(id, client) {
     channel.on('broadcast', channel.subscriptions[id]);
 });
 
+channel.on('leave', function(id){
+    channel.removeListener(
+        'broadcast', channel.subscriptions[id];
+        channel.emit('broadcast', id, id + ' has left the chat\n');
+    )
+});
+
+// Causes channel participants to be kicked off of the chat
+//  But it does not shut down the server
+channel.on('shutdown', function() {
+    channel.emit('broadcast', '', 'Channel has been shut down\n');
+    channel.removeAllListeners('broadcast');
+});
+
+// Display count of currently connected users and set max listeners
+channel.on('join', function(id, client) {
+    var welcome = 'Welcome\n'
+                + 'Guests online: ' + channel.listeners('broadcast').length;
+    client.write(welcome + '\n');
+    // Node displays a warning when there are
+    // more than 10 listeners on an event emitter.
+    channel.setMaxListeners(50);
+});
+
+
+
 var server = net.createServer(function(client) {
     var id = client.remoteAddress + ':' + client.remotePort;
     client.on('connect', function() {
         console.log('join');
         channel.emit('join', id, client);
     });
+
     client.on('data', function(data) {
         data = data.toString();
+        if (data == "shutdown\r\n") {
+            channel.emit('shutdown');
+        }
         channel.emit('broadcast', id, data);
+    });
+
+    client.on('close', function() {
+        channel.emit('leave', id);
     });
 });
 
 server.listen(8888);
 
+/*
+channel.on('leave', function(id){
+    channel.removeListener(
+        'broadcast', channel.subscriptions[id];
+        channel.emit('broadcast', id, id + ' has left the chat\n');
+    )
+});
 
 
